790ead426ec64b7fe9b2788d59baeca70f562042
Cerdan, Marcos <cerdanmarcos96@gmail.com>
2025-11-21 16:28:33 -0300

    actividades guia4

---
+++ 04 Estructuras Repetitivas/actividades guia4.py
@@ -0,0 +1,26 @@
+#ejercitacion unidad 4
+#ejercicio #1
+#def printer():
+#    for i in range(0,101):
+#        print(i)
+#
+#print(printer())
+
+
+#ejercicio #2
+#def printed_number():
+#   input_number = int(input("Ingrese un numero: "))
+#
+#    print(input_number, "posee {} digito/s".format(len(str(input_number))))
+#    return
+#print(printed_number())
+
+#ejercicio #3
+
+first_number = int(input("Ingrese un numero: "))
+second_number = int(input("Ingrese otro numero: "))
+suma = 0
+for i in range(first_number, second_number):
+    suma += i
+
+print("La suma de los numeros entre", first_number, "y", second_number, "es:", suma)

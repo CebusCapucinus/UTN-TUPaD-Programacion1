1f45e23ca7030fa8c5e078032888f812b5f2ddd7
Cerdan, Marcos <cerdanmarcos96@gmail.com>
2025-11-21 16:50:42 -0300

    avances

--- 04 Estructuras Repetitivas/actividades guia4.py
+++ 04 Estructuras Repetitivas/actividades guia4.py
@@ -17,10 +17,24 @@
 
 #ejercicio #3
 
-first_number = int(input("Ingrese un numero: "))
-second_number = int(input("Ingrese otro numero: "))
-suma = 0
-for i in range(first_number, second_number):
-    suma += i
+#first_number = int(input("Ingrese un numero: "))
+#second_number = int(input("Ingrese otro numero: "))
+#suma = 0
+#for i in range(first_number, second_number):
+#    suma += i
 
-print("La suma de los numeros entre", first_number, "y", second_number, "es:", suma)
+#print("La suma de los numeros entre", first_number, "y", second_number, "es:", suma)
+
+#ejercicio #4
+
+total = 0
+number = 1
+
+print("Ingrese los numeros que desea sumar. Ingrese 0 para finalizar.")
+
+while number != 0:
+    number = int(input("Numero: "))
+    total += number
+
+total -= 0
+print("Total acumulado:", total)    
